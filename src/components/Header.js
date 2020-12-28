2ae0c930b0df7e2b75d927e7072152ea288e0e8d
Ash Hellwig <ahellwig.dev@gmail.com>
2020-12-27 18:03:29 -0700

    templating

---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-diamond"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Ash Hellwig</h1>
+        <p>Cannabis Inventory and Compliance Professional</p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('experience')
+            }}
+          >
+            Experience
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('media')
+            }}
+          >
+            Media
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
