a7f0c45c41357de5904dfb3f841d5a2f632f90fa
Ash Hellwig <ahellwig.dev@gmail.com>
2020-12-27 18:36:35 -0700

    fix icon layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
       {/* <span className="icon fa-diamond"></span> */}
-      <img src={cannabis} alt=" " />
+      <img src={cannabis} alt=" " style={{ marginTop: '5px' }} height="55px" />
     </div>
     <div className="content">
       <div className="inner">
