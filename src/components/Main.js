3cf4c59a0d6a6a436f24523fe9bc6a7127a48081
Ash Hellwig <ahellwig.dev@gmail.com>
2021-11-28 22:32:16 -0700

    modify breakpoint condition for client list in experience for #2

--- src/components/Main.js
+++ src/components/Main.js
@@ -68,7 +68,7 @@ class Main extends React.Component {
           <div>
             <h3>Consulting and Development Clients</h3>
             <div>
-              {breakpoints[currentBreakpoint] >= breakpoints.mobileLandscape ? (
+              {breakpoints[currentBreakpoint] > breakpoints.mobileLandscape ? (
                 <ClientList />
               ) : (
                 <ClientListMobile />
@@ -132,28 +132,6 @@ class Main extends React.Component {
             <a href="mailto:ash@ashwigltd.com">ash@ashwigltd.com</a>&nbsp; with
             any business related inquiries.
           </p>
-          {/* <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form> */}
           <ul className="icons">
             <li>
               <a
