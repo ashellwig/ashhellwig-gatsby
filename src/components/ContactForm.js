74a9d542aed52c7079dde7044296ae84e239febd
Ash Hellwig <ahellwig.dev@gmail.com>
2021-12-03 02:26:17 -0700

    modify contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -17,88 +17,91 @@ import React from 'react'
 
 import '../assets/css/contact-form.css'
 
-const ContactForm = () => (
-  <div id="contact-form">
-    <form name="contact-form" method="POST" data-netlify="true">
-      {/* BEGIN: Netlify Preamble */}
-      <input type="hidden" name="bot-field" />
-      <input
-        type="hidden"
-        name="form-name"
-        value="ash.ashwigltd.com-contact-form"
-      />
-      {/* END: Netlify Preamble */}
-
-      {/* BEGIN: Form Field: First Name */}
-      <p>
-        <label
-          className="form-input-label"
-          htmlFor="contact-form-field-first-name"
+export default class ContactForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          name="contact"
         >
-          First Name
-        </label>
-        <input
-          type="text"
-          name="contact-form-field-first-name"
-          id="contact-form-field-first-name"
-        />
-      </p>
-      {/* END: Form Field: First Name */}
+          {/* BEGIN: Netlify Preamble */}
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          {/* END: Netlify Preamble */}
 
-      {/* BEGIN: Form Field: Last Name */}
-      <p>
-        <label
-          className="form-input-label"
-          htmlFor="contact-form-field-last-name"
-        >
-          Last Name
-        </label>
-        <input
-          type="text"
-          name="contact-form-field-last-name"
-          id="contact-form-field-last-name"
-        />
-      </p>
-      {/* END: Form Field: Last Name */}
+          {/* BEGIN: Form Field: First Name */}
+          <p>
+            <label
+              className="form-input-label"
+              htmlFor="contact-form-field-first-name"
+            >
+              First Name
+              <input
+                type="text"
+                name="contact-form-field-first-name"
+                id="contact-form-field-first-name"
+              />
+            </label>
+          </p>
+          {/* END: Form Field: First Name */}
 
-      {/* BEGIN: Form Field: Contact Email */}
-      <p>
-        <label
-          className="form-input-label"
-          htmlFor="contact-form-field-contact-email"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          name="contact-form-field-contact-email"
-          id="contact-form-field-contact-email"
-        />
-      </p>
-      {/* END: Form Field: Contact Email */}
+          {/* BEGIN: Form Field: Last Name */}
+          <p>
+            <label
+              className="form-input-label"
+              htmlFor="contact-form-field-last-name"
+            >
+              Last Name
+              <input
+                type="text"
+                name="contact-form-field-last-name"
+                id="contact-form-field-last-name"
+              />
+            </label>
+          </p>
+          {/* END: Form Field: Last Name */}
 
-      {/* BEGIN: Form Field: Message */}
-      <p>
-        <label
-          className="form-input-label"
-          htmlFor="contact-form-field-contact-message"
-        >
-          Message
-        </label>
-        <textarea
-          name="contact-form-field-contact-message"
-          id="contact-form-field-contact-message"
-        ></textarea>
-      </p>
-      {/* END: Form Field: Message */}
+          {/* BEGIN: Form Field: Contact Email */}
+          <p>
+            <label
+              className="form-input-label"
+              htmlFor="contact-form-field-contact-email"
+            >
+              Email
+              <input
+                type="email"
+                name="contact-form-field-contact-email"
+                id="contact-form-field-contact-email"
+              />
+            </label>
+          </p>
+          {/* END: Form Field: Contact Email */}
 
-      {/* BEGIN: Button: Submit */}
-      <p>
-        <button type="submit">Submit</button>
-      </p>
-      {/* END: Button: Submit */}
-    </form>
-  </div>
-)
+          {/* BEGIN: Form Field: Message */}
+          <p>
+            <label
+              className="form-input-label"
+              htmlFor="contact-form-field-contact-message"
+            >
+              Message
+              <textarea
+                name="contact-form-field-contact-message"
+                id="contact-form-field-contact-message"
+              ></textarea>
+            </label>
+          </p>
+          {/* END: Form Field: Message */}
 
-export default ContactForm
+          {/* BEGIN: Button: Submit */}
+          <p>
+            <button type="submit">Submit</button>
+          </p>
+          {/* END: Button: Submit */}
+        </form>
+      </div>
+    )
+  }
+}
