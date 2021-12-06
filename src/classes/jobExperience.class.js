cd0b8d47a55b254ba9c3979ec3500a7920ef97d0
Ash Hellwig <ahellwig.dev@gmail.com>
2021-12-05 23:11:01 -0700

    upgrade packages and add flow typing and babel

---
+++ src/classes/jobExperience.class.js
@@ -0,0 +1,4 @@
+export default class JobExperience {
+    str: companyName = '',
+
+};
---
+++ src/constants/jobExperience.js
@@ -0,0 +1,90 @@
+const TECH_CLEAR_CANNABIS_INC = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_NATTY_REMS = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_WOLFPAC_CANNABIS = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_IGADI_LTD = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_SMARTECH = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_MRS_GREENS_REMEDIES = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_APOLLO_DECENTRALIZED_ASSET_EXCHANGE = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+const TECH_IBM = {
+    companyName: '',
+    jobTitle: '',
+    location: '',
+    dateStart: '',
+    dateEnd: '',
+    description: '',
+    detailedAchievements: ''
+}
+
+export {
+    TECH_CLEAR_CANNABIS_INC,
+    TECH_NATTY_REMS,
+    TECH_WOLFPAC_CANNABIS,
+    TECH_IGADI_LTD,
+    TECH_SMARTECH,
+    TECH_MRS_GREENS_REMEDIES,
+    TECH_APOLLO_DECENTRALIZED_ASSET_EXCHANGE,
+    TECH_IBM
+}
