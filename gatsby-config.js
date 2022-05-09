5b749a6283953c9f962ac269dc6acec21be9e0c3
Ash Hellwig <ahellwig.dev@gmail.com>
2022-05-08 19:55:04 -0600

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,17 +40,30 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Ash Hellwig\'s Personal Site',
-        short_name: 'AshHellwig',
+        short_name: 'Ash Hellwig',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/images/logo-square.png', // This path is relative to the root of the site.
+        icons: [
+          {
+              src: "src/images/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png"
+          },
+          {
+              src: "src/images/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png"
+          }
+      ],
       },
     },
     'gatsby-plugin-sass',
   ],
   flags: {
-    DEV_SSR: false,
+    DEV_SSR: false
+    ,
   },
 }
