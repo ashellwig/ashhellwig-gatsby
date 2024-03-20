45bd26215b0ad2d99b0363243df146976b015381
Ash Hellwig <ahellwig.dev@gmail.com>
2024-03-20 12:22:03 -0600

    remove unused modules

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { useState } from 'react'
+import React from 'react'
 
 import '../assets/css/contact-form.css'
 
