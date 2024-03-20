2c9da45772f6aa9c0befd6c64d4401a921c285c0
Ash Hellwig <ahellwig.dev@gmail.com>
2024-03-20 11:20:28 -0600

    Add license headers

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
+// Copyright (C) 2024 Ash Hellwig
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import React from 'react'
 import ReactBreakpoints from 'react-breakpoints'
 
@@ -5,9 +20,6 @@ import Layout from '../components/layout'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
-
-// import '../assets/css/client-list.css'
-// import '../assets/css/client-list-mobile.css'
 
 const breakpoints = {
   mobile: 320,
